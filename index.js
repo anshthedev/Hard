4a2ffe7a181b01be3ab108297af6ad140b8b664d
Ansh <anshkool1104@gmail.com>
2024-04-20 20:25:48 -0500

    Removing Unneeded Parts

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-console.log("There are three people working in the space station")
-console.log("Pilot is driving the space station")
-console.log("Crewmate 1 is stationary")
-console.log("Crewmate 2 is stationary")
-console.log("Driving Station, Take Out Garbage, and Clean Station are always happening")
-
-
-
-// Function to handle assigning a task to a crewmate
 function assignTask(crewmateButton, crewmateId) {
     var task = document.getElementById('taskInput').value.trim();
-    
-    // Check if task is provided
+
     if (task) {
         crewmateButton.disabled = true;
         crewmateButton.classList.add('assigned');
@@ -20,27 +10,55 @@ function assignTask(crewmateButton, crewmateId) {
         taskButton.classList.add('task');
         document.getElementById('tasksContainer').appendChild(taskButton);
     
-        var countdown = 10; // Adjust countdown duration as needed (in seconds)
+        var countdown = 10;
         var countdownInterval = setInterval(function() {
             countdown--;
             taskButton.innerText = task + ' (Time remaining: ' + countdown + 's)';
             if (countdown <= 0) {
                 clearInterval(countdownInterval);
                 taskButton.parentNode.removeChild(taskButton);
-                // Re-enable crewmate button when task is completed or removed
                 crewmateButton.disabled = false;
                 crewmateButton.classList.remove('assigned');
             }
         }, 1000);
     }
 }
+function addMission() {
+  var missionInput = document.getElementById('missionInput');
+  var mission = missionInput.value.trim();
 
-  
-  // Function to handle adding crewmate buttons
+  if (mission) {
+      var missionElement = document.createElement('p');
+      missionElement.innerText = mission;
+      missionElement.classList.add('mission');
+      document.getElementById('missionsContainer').appendChild(missionElement);
+
+      missionInput.value = '';
+  } else {
+      alert('Please enter a mission before adding.');
+  }
+}
+
+function addTask() {
+    var taskInput = document.getElementById('taskInput');
+    var task = taskInput.value.trim();
+
+    if (task) {
+        var taskButton = document.createElement('button');
+        taskButton.innerText = task;
+        taskButton.classList.add('task');
+        document.getElementById('tasksContainer').appendChild(taskButton);
+
+        taskInput.value = '';
+    } else {
+        alert('Please enter a task before adding.');
+    }
+}
+
   function addButton() {
     var buttonContainer = document.getElementById('buttonContainer');
     var newButton = document.createElement('button');
-    var crewmateCount = document.querySelectorAll('.crewmate').length + 1-4;
+    var crewmateCount = document.querySelectorAll('.crewmate').length + 1-1;
     newButton.setAttribute('class', 'crewmate');
     newButton.innerHTML = 'Crewmate ' + crewmateCount;
     newButton.onclick = function() {
@@ -48,9 +66,7 @@ function assignTask(crewmateButton, crewmateId) {
     };
     buttonContainer.appendChild(newButton);
   }
-  
-  // Function to handle removing crewmate buttons
-  
+
   function removeButton() {
     var buttonContainer = document.getElementById('buttonContainer');
     var crewmateButtons = document.getElementsByClassName('crewmate');
@@ -67,23 +83,4 @@ function assignTask(crewmateButton, crewmateId) {
   function lift(){
     landing.innerHTML = `
     <p1>Currently In Air</p1>`;
-  }
-
-  
-  function addMission() {
-    var missionInput = document.getElementById('missionInput');
-    var mission = missionInput.value.trim();
-  
-    if (mission) {
-        // Create a mission display element
-        var missionElement = document.createElement('p');
-        missionElement.innerText = mission;
-        missionElement.classList.add('mission');
-        document.getElementById('missionsContainer').appendChild(missionElement);
-  
-        // Clear the input after adding the mission
-        missionInput.value = '';
-    } else {
-        alert('Please enter a mission before adding.');
-    }
   }
